249d14badd2a90ffd6a1321bf41ad7391b83ff2f
Gian Dal Pont <giandalpont@gmail.com>
2019-11-09 13:21:36 -0200

    Cadastro de usuários

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,9 @@
 import { Router } from 'express';
 
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-    const user = await User.create({
-        name: 'Gian Dal Pont',
-        email: 'gian@gmail.com',
-        password_hash: '12323423445',
-    });
-
-    return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
